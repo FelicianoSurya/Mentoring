cbcb18251ad7c20cc2863c3a809dd8b1f27e64e9
55213-Geraldo Nathanael <geraldo.nathanael@student.umn.ac.id>
2022-07-03 12:27:22 +0700

    @:valuepage
    valuepage 03/07/2022

--- resources/views/page/values.blade.php
+++ resources/views/page/values.blade.php
@@ -7,47 +7,51 @@
 
 @section('content')
 <div class="body">
-<div class="section1">
-        <img class="bg bg1 absolute" src="{{asset('Images/Valuepage/BG 1_Value.png')}}">
-        <div class="content relative text-center">
-            <img class="logo5c" src="{{asset('Images/Valuepage/Logo 5C.png')}}" />
+    <div class="section1 relative">
+        <img class="bg bg1 d-none d-sm-block absolute" src="{{asset('Images/Valuepage/BG_1_Value.png')}}" />
+        <img class="bg bg1 d-sm-none absolute" src="{{asset('Images/Valuepage/BG_Responsive.png')}}" />
+        <div class="konten relative text-center">
+            <img class="logo5c" src="{{asset('Images/Valuepage/Logo_5C.png')}}" />
             <h1 class="judul blue_shadow bold">VALUE MENTORING</h1>
             <h3 class="umn_2022 medium">UMN 2022</h3>
             <img class="maskot absolute"  src="{{asset('Images/Valuepage/Maskot_Value.png')}}" />
         </div>
     </div>
-    <div class="section2">
-        <div class="relative content text-center">
-            <div>
-                <img class="absolute droplet" src="{{asset('Images/Valuepage/Droplet_5C UMN.png')}}" />
+    <div class="section2 relative">
+        <div class="konten text-center">
+            <div class="relative">
+                <img class="droplet" src="{{asset('Images/Valuepage/Droplet_5C_UMN.png')}}" />
                 <h1 class="judul lightblue_shadow bold">5C UMN</h1>
             </div>
+            <div>
+                <p class="arti medium d-lg-none">*click the logo to see other values</p>
+            </div>
             <div class="row mx-5">
-                <div class="col-1">      
+                <div class="col-1 d-none d-md-block">      
                 </div>
-                <div class="col-2">
-                    <img class="nilai5c" src="{{asset('Images/Valuepage/Logo Caring.png')}}">
+                <div class="col-2 mr-2">
+                    <img class="nilai5c" src="{{asset('Images/Valuepage/Logo_Caring.png')}}">
                 </div>
-                <div class="col-2">
-                    <img class="nilai5c" src="{{asset('Images/Valuepage/Logo Credible.png')}}">
+                <div class="col-2 mx-2">
+                    <img class="nilai5c" src="{{asset('Images/Valuepage/Logo_Credible.png')}}">
                 </div>
-                <div class="col-2">
-                    <img class="nilai5c " src="{{asset('Images/Valuepage/Logo Competent.png')}}">
+                <div class="col-2 mx-2">
+                    <img class="nilai5c " src="{{asset('Images/Valuepage/Logo_Competent.png')}}">
                 </div>
-                <div class="col-2">
+                <div class="col-2 mx-2">
                     <img class="nilai5c" src="{{asset('Images/Valuepage/Competitive.png')}}">
                 </div>
-                <div class="col-2">
-                    <img class="nilai5c" src="{{asset('Images/Valuepage/Logo Customer delight.png')}}">
+                <div class="col-2 ml-2">
+                    <img class="nilai5c" src="{{asset('Images/Valuepage/Logo_ Customerdelight.png')}}">
                 </div>
-                <div class="col-1">
+                <div class="col-1 d-none d-md-block">
                 </div>
             </div>
             <div class="row justify-content-center mt-5 nilai5c-box">
                 
             </div>
         </div>
-        <img class="bg bg2 absolute" src="{{asset('Images/Valuepage/BG 2_Value.png')}}"/>
+        <img class="bg bg2" src="{{asset('Images/Valuepage/BG_2_Value.png')}}"/>
     </div>
 </div>
 @endsection
@@ -58,35 +62,35 @@
 const meaning = [
             {
                 nilai: "CARING",
-                logo: "{{asset('Images/Valuepage/Logo5C-caring.png')}}",
+                logo: "{{asset('Images/Valuepage/Logo5c_Caring.png')}}",
                 arti: "Hati yang Terbuka",
                 penjelasan: "Sikap peduli terhadap mahasiswa, para dosen dan staff, lingkungan sekitar kampus, serta masyarakat sekitar. ",
                 class: "position-caring"
             },
             {
                 nilai: "CREDIBLE",
-                logo:"{{asset('Images/Valuepage/Logo5C-credible.png')}}",
+                logo:"{{asset('Images/Valuepage/Logo5c_Credible.png')}}",
                 arti: "Ruang Integritas",
                 penjelasan:"Dapat dipercaya dan diandalkan melalui integritas dalam melaksanakan kewajibannya, aktif berorganisasi dengan mengikuti kegiatan kampus UMN, serta menjaga nama baik UMN.",
                 class: "position-credible"
             },
             {
                 nilai: "COMPETENT",
-                logo:"{{asset('Images/Valuepage/Logo5C-competent.png')}}",
+                logo:"{{asset('Images/Valuepage/Logo5c_Competent.png')}}",
                 arti: "Gedung Pencakar Langit",
                 penjelasan:"Aktif dalam perkuliahan, mengikuti organisasi untuk mengembangkan soft skills, serta mencari referensi untuk menambah pengetahuan.",
                 class: "position-competent"
             },
             {
                 nilai: "COMPETITIVE",
-                logo:"{{asset('Images/Valuepage/Logo5C-competitive.png')}}",
+                logo:"{{asset('Images/Valuepage/Logo5c_Competitive.png')}}",
                 arti: "Podium",
                 penjelasan:"Berkompetisi secara sehat dalam berbagai kompetisi yang ada dan memberikan ide-ide baru yang mengembangkan kemampuan mahasiswa serta lingkungannya.",
                 class: "position-competitive"
             },
             {
                 nilai: "CUSTOMER DELIGHT",
-                logo:"{{asset('Images/Valuepage/Logo5c-customerdelight.png')}}",
+                logo:"{{asset('Images/Valuepage/Logo5c_Customerdelight.png')}}",
                 arti: "Tangga Naik ke Atas",
                 penjelasan:"Sikap berorientasi dan antisipatif terhadap kebutuhan orang tua dengan belajar, berprestasi, dan sopan selama berada di kampus. ",
                 class: "position-customerdelight"
@@ -96,9 +100,11 @@ const meaning = [
         const buttons = document.querySelectorAll(".nilai5c");
         const nilai5c = document.querySelector(".nilai5c-box");
         let box_nilai = document.createElement("div");
-
+        
         buttons.forEach((e, index) => {
             e.addEventListener("click",() =>{
+                buttons.forEach((btn) => btn.classList.remove('active'));
+                e.classList.add('active');
                 box_nilai.innerHTML = `
                 <h1 class="bold lightblue_shadow nilai">
                     ${meaning[index].nilai}
