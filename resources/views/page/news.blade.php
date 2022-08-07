fe50c6f767f48738af4579f0bc61ef658d47a4d1
Vallencius <vallencius1901@gmail.com>
2022-08-07 20:18:57 +0700

    News Update

--- resources/views/page/news.blade.php
+++ resources/views/page/news.blade.php
@@ -36,21 +36,21 @@
             <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                 <div class="carousel-inner news-carousel">
                   <div class="carousel-item active">
-                    <img src="{{ asset('Images/News/Frame 17.png') }}" class="d-block w-75 mx-auto">
+                    <img src="{{ asset('Images/News/Pembekalan 1.jpg') }}" class="d-block w-75 mx-auto carousel-img">
                     <div class="carousel-caption carousel-desc-1">
                       <h5 class="carousel-caption-header">Pembekalan Mentoring Day 1</h5>
                       <p class="carousel-caption-desc">Pada Pembekalan Mentor 1 ini, narasumber mengangkat tema “5C with Diagram”. 5C sendiri merupakan nilai-nilai yang ingin ditanamkan oleh Universitas Multimedia Nusantara (UMN) kepada seluruh mahasiswanya agar menjadi lebih Caring, Credible, Competent, Competitive, dan Customer Delight. Nah, nilai-nilai tersebut disampaikan secara komprehensif oleh tim dari Diagram Kompas Gramedia kepada para Mentor untuk disampaikan kembali kepada para Mentee pada kegiatan Mentoring UMN 2022.</p>
                     </div>
                   </div>
                   <div class="carousel-item">
-                    <img src="{{ asset('Images/News/Frame 19.png') }}" class="d-block w-75 mx-auto">
+                    <img src="{{ asset('Images/News/Pembekalan 2.jpg') }}" class="d-block w-75 mx-auto carousel-img">
                     <div class="carousel-caption">
                       <h5 class="carousel-caption-header">Pembekalan Mentoring Day 2</h5>
                       <p class="carousel-caption-desc">Pada Pembekalan Mentor 2 ini, narasumber mengangkat tema “How to be an Ideal Mentor”. Beberapa pertanyaan pasti muncul dari dalam benak seorang Mentor saat ia mendengar kata “Ideal Mentor”. Misalnya, mentor yang ideal itu seperti apa, sih? Bagaimana caranya untuk menjadi Mentor yang ideal? Dan aspek apa saja yang harus dimiliki oleh seorang Mentor untuk menjadi ideal? Nah, beberapa pertanyaan tersebut dijawab secara komprehensif oleh pembimbing Mentoring UMN 2022, yaitu Mas Sonny Tirta, M.Psi. Tidak hanya itu, Mas Sonny juga turut berperan sebagai motivator bagi para Mentor yang baru terjun ke dunia Mentorship.</p>
                     </div>
                   </div>
                   <div class="carousel-item">
-                    <img src="{{ asset('Images/News/Frame 19.png') }}" class="d-block w-75 mx-auto">
+                    <img src="{{ asset('Images/News/Pembekalan 3.jpg') }}" class="d-block w-75 mx-auto carousel-img">
                     <div class="carousel-caption">
                       <h5 class="carousel-caption-header">Pembekalan Mentoring Day 3</h5>
                       <p class="carousel-caption-desc">Pada Pembekalan Mentor 3 ini, narasumber mengangkat tema “Mentor Starter Pack”. Nah, apa sih bedanya dengan tema pada Pembekalan Mentor 2? Kalau pada Pembekalan Mentor 2 secara khusus membahas mengenai cara Mentor untuk menjadi ideal, pada Pembekalan Mentor 3 yang dijelaskan adalah bekal yang harus mereka miliki saat menjadi seorang Mentor. Salah satu yang paling utama adalah kepercayaan diri dan kemampuan public speaking yang secara komprehensif disampaikan oleh salah satu dosen Komunikasi Strategis di UMN, yaitu Kak Vega Karina Andira Putri, S.Sos., M.Si.</p>
@@ -74,14 +74,17 @@
 
         {{-- SECTION-3 --}}
         <div class="section-3">
-            <a href="https://www.youtube.com" class="d-block mx-auto video-mentoring height-40"></a>
   
             {{-- IMAGE --}}
             <img class="vector-3-top" src="{{ asset('Images/News/BG 3_News-Top.png') }}">
             <img class="vector-3-bottom" src="{{ asset('Images/News/BG 3_News-Bottom.png') }}">
             <img class="trail-3" src="{{ asset('Images/News/Trail 3_News.png') }}">
             <img class="video-border" src="{{ asset('Images/News/Video border.png') }}">
-            <img src="{{ asset('Images/News/Frame 20.png') }}" class="d-block mx-auto video-mentoring">
+            <video autoplay controls class="d-block mx-auto video-mentoring">
+              <source src="{{ asset('Images/News/VideoPembekalan.mp4') }}" type="video/mp4">
+              Your browser does not support the video tag.
+            </video>
+            
           </div>
         {{-- END OF SECTION-3 --}}
 
@@ -100,11 +103,11 @@
   
             {{-- IMAGE --}}
             <div class="relative box-news">
-              <img src="{{ asset('Images/News/Frame 19.png') }}" class="news-hero">
-              <img src="{{ asset('Images/News/Frame 19.png') }}" class="news-child left-img">
-              <img src="{{ asset('Images/News/Frame 19.png') }}" class="news-child">
-              <img src="{{ asset('Images/News/Frame 19.png') }}" class="news-child left-img">
-              <img src="{{ asset('Images/News/Frame 19.png') }}" class="news-child">
+              <img src="{{ asset('Images/News/Pembekalan 1.jpg') }}" class="news-hero">
+              <img src="{{ asset('Images/News/Pembekalan 2.jpg') }}" class="news-child left-img">
+              <img src="{{ asset('Images/News/Pembekalan 3.jpg') }}" class="news-child">
+              <img src="{{ asset('Images/News/Pembekalan 1.jpg') }}" class="news-child left-img">
+              <img src="{{ asset('Images/News/Pembekalan 2.jpg') }}" class="news-child">
               <button class="btn btn-seemore see-more">See More</button>
             </div>
 
